f776d7e031898e14c455bc7c4b624e29b918a061
Matt Carter <m@ttcarter.com>
2019-02-28 11:00:29 +1000

    Added test for promise resolution sequences

--- test/test.js
+++ test/test.js
@@ -19,6 +19,28 @@ describe('simple scenario tests', ()=> {
 			.then(()=> expect(called).to.deep.equal(['foo', 'bar']))
 	});
 
+	it('should fire promise chains correctly', ()=> {
+		var lastPromise;
+
+		var emitter =  eventer.extend({});
+
+		emitter
+			.on('foo', ()=> expect(lastPromise).to.not.be.ok)
+			.on('foo', ()=> lastPromise = 'foo')
+			.on('foo', ()=> new Promise(resolve => setTimeout(resolve, 300)))
+			.on('bar', ()=> expect(lastPromise).to.be.equal('foo'))
+			.on('bar', ()=> lastPromise = 'bar')
+			.on('bar', ()=> new Promise(resolve => setTimeout(resolve, 200)))
+			.on('baz', ()=> expect(lastPromise).to.be.equal('bar'))
+			.on('baz', ()=> lastPromise = 'bar')
+			.on('baz', ()=> new Promise(resolve => setTimeout(resolve, 100)))
+
+		return Promise.resolve()
+			.then(()=> emitter.emit('foo'))
+			.then(()=> emitter.emit('bar'))
+			.then(()=> emitter.emit('baz'))
+	});
+
 	it('should collect subscriber results', ()=> {
 		var called = [];
 		var emitter =  eventer.extend({});
